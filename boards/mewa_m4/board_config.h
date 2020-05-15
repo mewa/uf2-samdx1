a000508550609edada7aca516530ecde304a8e3d
Marcin Chmiel <marcin.k.chmiel@gmail.com>
2020-05-15 03:03:07 +0200

    Add SAMD51G18A board

---
+++ boards/mewa_m4/board_config.h
@@ -0,0 +1,12 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define CRYSTALLESS    1
+
+#define VENDOR_NAME "mewa"
+#define PRODUCT_NAME "mewaboard"
+#define VOLUME_LABEL "MEWA"
+#define INDEX_URL "https://example.com"
+#define BOARD_ID "SAMD51G18A-mewa-v1"
+
+#endif
